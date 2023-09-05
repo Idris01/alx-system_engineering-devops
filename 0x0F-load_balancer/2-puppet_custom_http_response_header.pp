2dbca6d3a3c59e326c20c014e3b232c922075ee1
Idris Adebowale <idrys01@gmail.com>
2023-09-05 08:54:38 +0000

    set headers only

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,61 +4,19 @@ class nginx_installation {
     package { 'nginx':
 	ensure => 'installed',
     }
-    
-    file { '/var/www/html/index.html':
-	ensure => 'present',
-	owner => 'root',
-	group => 'root',
-        require => Package['nginx'],
-	content => "Hello World!",
-	}
-
-    file { '/usr/share/nginx/html/custom_404.html':
-	ensure => 'present',
-	owner => 'root',
-	group => 'root',
-        require => Package['nginx'],
-	content => "Ceci n'est pas une page",
-	}
-
-    file { '/etc/nginx/sites-available/default':
+   
+    file { '/etc/nginx/conf.d/global_headers.conf':
 	ensure => 'file',
 	owner => 'root',
 	group => 'root',
         require => Package['nginx'],
-	content => "
-
-server {
-    listen 80;
-    server_name _;
-
-    root /var/www/html;
-
-    add_header X-Served-By \$HOSTNAME;
-
-    index index.html index.htm index.nginx-debian.html;
-
-    location /redirect_me {
-        return 301 http://www.idrisoft.tech;
+	content => "add_header X-Served-By \$HOSTNAME;",
     }
-
-    error_page 404 /custom_404.html;
-    location = /custom_404.html {
-	root /usr/share/nginx/html;
-	internal;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-    ",
-    }
-    
+   
     service { 'nginx':
 	ensure => 'running',
 	enable => true,
-	subscribe => File['/etc/nginx/sites-available/default'],
+	require => File['/etc/nginx/conf.d/global_headers.conf'],
     }
 }
 
