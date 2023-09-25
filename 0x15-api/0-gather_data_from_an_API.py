d8eb6d94d967db187a305b30326e46b090d4ba08
Idris Adebowale <idrys01@gmail.com>
2023-09-25 03:18:01 -0700

    add README file and script that shows user todo lists

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Fetch todos of a given user
+"""
+import urllib.request as request
+import json
+import sys
+
+url1 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        sys.argv[1])
+url2 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+
+if __name__ == '__main__':
+    resp1 = request.urlopen(url1)
+    resp2 = request.urlopen(url2)
+
+    user = json.loads(resp2.read())
+    todos = json.loads(resp1.read())
+
+    done = (int(todo['completed']) for todo in todos)
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), sum(done), len(todos)))
+    for todo in todos:
+        if todo['completed']:
+            print("\t{}".format(todo.get("title")))
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# API (Application Programming Interface)
+This project deals with tasks involving API,fetching data using the `urllib.request`
