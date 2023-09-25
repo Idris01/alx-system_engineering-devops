ce20b02527e7f1ece7c558427ffa89d3a3c4cae6
Idris Adebowale <idrys01@gmail.com>
2023-09-25 06:43:59 -0700

    adjust code based on requirements

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Fetch todos of a given user
+"""Exports todo list of a given user to csv
 """
-import urllib.request as request
 import json
 import sys
+from urllib import request
 
-url1 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        sys.argv[1])
-url2 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
 
 if __name__ == '__main__':
+    url1 = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        sys.argv[1])
+    url2 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+
     resp1 = request.urlopen(url1)
     resp2 = request.urlopen(url2)
 
