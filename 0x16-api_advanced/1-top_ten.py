acf2f1eaf825b1840cbed9d55d5e1b359070ce81
Idris Adebowale <idrys01@gmail.com>
2023-10-10 16:58:34 +0000

    add script that print the title of top ten posts in a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Prints title of top ten posts of a given reddit
+"""
+
+import json
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    """Prints title of top ten posts of a subreddit
+    """
+    url_template = 'https://www.reddit.com/r/{}/top.json?limit=10'
+
+    try:
+        resp = requests.get(
+                url_template.format(subreddit),
+                headers={'User-Agent': 'idrisoft'},
+                allow_redirects=False)
+        data = json.loads(resp.text)
+        for post in data['data']['children']:
+            print(post['data'].get('title'))
+    except Exception as e:
+        print(None)
+
+
+if __name__ == '__main__':
+    top_ten(sys.argv[1])
