e3b9e38e827989f02559d08cf224b1cd9951969c
Idris Adebowale <idrys01@gmail.com>
2023-10-11 18:22:44 +0000

    add comment to manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Debugs a LAMP stack web server
+
 exec {'debug':
   command   => '/bin/bash -c "sed -i s/phpp/php/g /var/www/html/wp-settings.php && service apache2 restart"',
   provider  => 'shell',
