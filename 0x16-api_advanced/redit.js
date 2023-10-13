b9f2499a42cab22213100a4db4b95a7e23b92383
Idris Adebowale <idrys01@gmail.com>
2023-10-12 21:36:20 -0700

    remove junk file

--- 0x16-api_advanced/redit.js
+++
@@ -1,5 +0,0 @@
-const request = require("request");
-const options = {url:'https://www.reddit.com/api/me.json',headers: {'User-Agent':'refidas'}};
-request(options, (err, resp, body) =>{
-		if (!err) console.log(JSON.parse(body));
-	});
